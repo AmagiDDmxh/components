97031628932c2fe3c989c7de245f52c17e329c72
amagi <amagiddmxh@qq.com>
2021-01-11 04:40:20 +0800

    [ fix ] ButtonDrip undefined current

--- src/components/Button/ButtonDrip.tsx
+++ src/components/Button/ButtonDrip.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useLayoutEffect, useRef } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { css } from '@emotion/css'
 import { keyframes } from '@emotion/react'
 
@@ -26,6 +26,25 @@ const expand = keyframes`
   }
 `
 
+const dripCls = css({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+})
+
+const animCls = ({ top, left }: { top: number, left: number }) =>
+  css({
+    top,
+    left,
+    position: 'absolute',
+    animation: `350ms ease-in ${expand}`,
+    animationFillMode: 'forwards',
+    width: '1rem',
+    height: '1rem',
+  })
+
 const ButtonDrip: FC<Props> = props => {
   const { x = 0, y = 0, color = 'rgba(0, 0, 0, 0.65)', onCompleted } = props
   const dripRef = useRef<HTMLDivElement>(null)
@@ -33,39 +52,24 @@ const ButtonDrip: FC<Props> = props => {
   const top = y - 10
   const left = x - 10
 
-  useLayoutEffect(() => {
-    if (!dripRef.current) return
-    dripRef.current.addEventListener('animationend', onCompleted)
+  useEffect(() => {
+    const { current } = dripRef
+    if (!current) return
+    current.addEventListener('animationend', onCompleted)
     return () => {
-      if (!dripRef.current) return
-      dripRef.current.removeEventListener('animationend', onCompleted)
+      if (!current) return
+      current.removeEventListener('animationend', onCompleted)
     }
   })
 
   return (
-    <div
-      ref={dripRef}
-      className={css({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-      })}>
+    <div ref={dripRef} className={dripCls}>
       <svg
         width="20"
         height="20"
         viewBox="0 0 20 20"
         style={{ top, left }}
-        className={css({
-          top,
-          left,
-          position: 'absolute',
-          animation: `350ms ease-in ${expand}`,
-          animationFillMode: 'forwards',
-          width: '1rem',
-          height: '1rem',
-        })}>
+        className={animCls({ top, left })}>
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
           <g fill={color}>
             <rect width="100%" height="100%" rx="10" />
